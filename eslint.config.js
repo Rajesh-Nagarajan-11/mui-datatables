517fd0a5685081e000fd21fc9c2bdf004d4ae039
YASHMAHAKAL <yvsst01@gmail.com>
2026-04-10 02:49:51 +0530

    fix: restore npm run dev
    Signed-off-by: YASHMAHAKAL <yvsst01@gmail.com>

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+const pluginReact = require('eslint-plugin-react');
+const pluginJsxA11y = require('eslint-plugin-jsx-a11y');
+const pluginReactHooks = require('eslint-plugin-react-hooks');
+const globals = require('globals');
+
+module.exports = [
+  {
+    files: ['src/**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.mocha,
+        ...globals.es2020,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      'jsx-a11y': pluginJsxA11y,
+      'react-hooks': pluginReactHooks,
+    },
+    rules: {
+      ...pluginJsxA11y.configs.recommended.rules,
+      'no-console': 'off',
+      semi: 2,
+      'no-undef': 2,
+      'no-undef-init': 2,
+      'no-tabs': 2,
+      'react/self-closing-comp': 2,
+      'react/no-typos': 2,
+      'react/jsx-no-duplicate-props': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'jsx-a11y/no-autofocus': [
+        2,
+        {
+          ignoreNonDOM: true,
+        },
+      ],
+    },
+  },
+];
